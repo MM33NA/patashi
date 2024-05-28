794293c6f946d681f5627dade32a25058b0b46ce
MM33NA <mmaharjan039@gmail.com>
2024-05-28 01:40:52 -0500

    Updates

---
+++ static/js/952.b5e1a983.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpatashi=self.webpackChunkpatashi||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.b5e1a983.chunk.js.map
