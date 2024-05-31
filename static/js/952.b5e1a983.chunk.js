db3800c73b513f641a3f832ac67e3734bc29fb5d
Work <work@192-168-1-5.tpgi.com.au>
2024-05-31 14:43:25 +1000

    Updates

--- static/js/952.b5e1a983.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpatashi=self.webpackChunkpatashi||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.b5e1a983.chunk.js.map
